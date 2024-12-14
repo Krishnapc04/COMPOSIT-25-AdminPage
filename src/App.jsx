60342ec68655c51a2891dfedbfec03338698f945
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2024-12-14 13:32:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const  App = () => {
+  return (
+    <>
+      <h1 className='text-red-500 text-3xl text-center'>This is COMPOSIT'25 Admin page</h1>
+    </>
+  )
+}
+
+export default App
