8927199285640b5108355944425a274f0274c999
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2025-01-28 13:22:50 +0530

    fuzzy search added

--- src/const.jsx
+++ src/const.jsx
@@ -1,4 +1,4 @@
-const BaseUrl = "http://localhost:5000"
-// const BaseUrl = "https://composit-25-server.onrender.com"
+// const BaseUrl = "http://localhost:5000"
+const BaseUrl = "https://composit-25-server.onrender.com"
 
 export default BaseUrl
