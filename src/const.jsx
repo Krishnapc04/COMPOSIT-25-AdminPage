92db3fdddc8bb42573046a85e4cecc2e0c3dfb22
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2025-03-03 16:32:35 +0530

    const changed

--- src/const.jsx
+++ src/const.jsx
@@ -1,5 +1,5 @@
-const BaseUrl = "http://localhost:5000"
-// const BaseUrl = "https://api.composit.in"
+// const BaseUrl = "http://localhost:5000"
+const BaseUrl = "https://api.composit.in"
 // const BaseUrl = "https://composit-25-server.onrender.com"
 
 export default BaseUrl
